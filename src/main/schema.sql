91316cf3962fd28a3ad60b0dcccd1c5ce5fe4881
João Gabriel <joaogabrielpeixoto2005@gmail.com>
2026-05-30 11:27:39 -0300

    feat: add SQL schema to project

---
+++ src/main/schema.sql
@@ -0,0 +1,65 @@
+CREATE DATABASE universidade;
+USE universidade;
+
+CREATE TABLE usuario (
+  id_usuario INTEGER PRIMARY KEY AUTO_INCREMENT,
+  nome TEXT,
+  senha TEXT,
+  endereco TEXT,
+  tipo ENUM('Aluno', 'Prof', 'Admin')
+);
+
+CREATE TABLE professor (
+    cpf VARCHAR(11) PRIMARY KEY DEFAULT '00000000000',
+    id_usuario INTEGER,
+    CONSTRAINT fk_usuario_prof
+    FOREIGN KEY (id_usuario)
+    REFERENCES usuario(id_usuario)
+);
+
+CREATE TABLE aluno (
+    matricula BIGINT PRIMARY KEY,
+    id_usuario INTEGER,
+    CONSTRAINT fk_usuario_aluno
+    FOREIGN KEY (id_usuario)
+    REFERENCES usuario(id_usuario)
+);
+
+CREATE TABLE disciplina (
+    id_disciplina INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nome TEXT,
+    codigo TEXT
+);
+
+CREATE TABLE turma (
+    id_turma INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id_disciplina INTEGER,
+    cpf_professor VARCHAR(11),
+    aulas_ministradas INTEGER,
+    local_de_aula TEXT,
+    horario TEXT,
+    ativo BOOL,
+    CONSTRAINT fk_professor_turma
+    FOREIGN KEY (cpf_professor)
+    REFERENCES professor(cpf),
+    CONSTRAINT fk_disciplina_turma
+    FOREIGN KEY (id_disciplina)
+    REFERENCES disciplina(id_disciplina)
+);
+
+CREATE TABLE indice (
+    id_indice INTEGER PRIMARY KEY AUTO_INCREMENT,
+    matricula_aluno BIGINT,
+    id_turma INTEGER,
+    nota1 INTEGER,
+    nota2 INTEGER,
+    nota3 INTEGER,
+    faltas INTEGER,
+    estado ENUM('Matr', 'Apr', 'Rep', 'Canc'),
+    CONSTRAINT fk_aluno_indice
+    FOREIGN KEY (matricula_aluno)
+    REFERENCES aluno(matricula),
+    CONSTRAINT fk_turma_indice
+    FOREIGN KEY (id_turma)
+    REFERENCES turma(id_turma)
+);
