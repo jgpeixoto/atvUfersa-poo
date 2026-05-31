c5417c728633bebea334ba617dfa34e74468f7dc
jgpeixoto <joaogabrielpeixoto2005@gmail.com>
2026-05-31 14:57:00 -0300

    refactor: restructure project and add docker-compose

--- src/main/java/br/edu/ufersa/Main.java
+++ src/main/java/br/edu/ufersa/Main.java
@@ -1,7 +1,14 @@
 package br.edu.ufersa;
 
-public class Main {
-    public static void main(String[] args) {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/universidade", "root", "admin");
+        System.out.println("Conectado!");
+        con.close();
     }
 }
